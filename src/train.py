d3f19f8477732fddd187c39313ab250cb5e67692
John Stark <starkja82@gmail.com>
2024-02-03 17:44:53 -0500

    Increased initial random timesteps

--- src/train.py
+++ src/train.py
@@ -63,7 +63,7 @@ def main(argv):
     #print("///// Explore config:\n", pretty_print(explore_config))
     explore_config["type"]                      = "GaussianNoise" #default OrnsteinUhlenbeckNoise doesn't work well here
     explore_config["stddev"]                    = 0.25 #this param is specific to GaussianNoise
-    explore_config["random_timesteps"]          = 10_000 #provides random experiences to pre-populate the experience buffer
+    explore_config["random_timesteps"]          = 100_000 #provides random experiences to pre-populate the experience buffer
     explore_config["initial_scale"]             = 1.0
     explore_config["final_scale"]               = 0.1
     explore_config["scale_timesteps"]           = 50_000_000
